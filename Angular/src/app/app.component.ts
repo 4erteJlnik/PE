c149f670001a660d98abf2dadace45893f734c77
Araslanova D <daria.araslanova@gmail.com>
2022-06-23 17:23:14 +0700

    Revert "a"
    This reverts commit a0edc4c4794a6a6dbf3bcbcc71f68c7c7b0d8f93.

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,11 +1,11 @@
 ﻿import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app',
+    template: `<label>Введите имя:</label>
+                 <input [(ngModel)]="name" placeholder="name">
+                 <h2>Добро пожаловать {{name}}!</h2>`
 })
-
 export class AppComponent {
     name = '';
 }
--- Angular/src/app/store/Post.ts
+++
@@ -1,6 +0,0 @@
-export class Post{
-    public date:Date | undefined;
-    constructor(public name:string, public cost:Number,public dateOfCreate:string,public id:string,public creatorId:string){
-    
-    }
-}
